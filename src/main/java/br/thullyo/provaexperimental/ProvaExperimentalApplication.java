cfd410c11bdb053dfb96633b1f601df2b01d8624
Thullyoo <thullyofilho@gmail.com>
2024-10-01 16:34:32 -0300

    first commit

---
+++ src/main/java/br/thullyo/provaexperimental/ProvaExperimentalApplication.java
@@ -0,0 +1,13 @@
+package br.thullyo.provaexperimental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaExperimentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaExperimentalApplication.class, args);
+    }
+
+}
